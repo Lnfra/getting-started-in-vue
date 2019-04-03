5af4c435155a4052374243ec0807ac2ee38170e9
Angeline <atan@thoughtworks.com>
2019-04-03 10:46:02 +0800

    CalculatorApp implementation

--- src/App.vue
+++ src/App.vue
@@ -1,61 +1,59 @@
 <template>
   <div id="app">
-    <my-counter ref="counter" v-for="item in items"
-      :key="item.label"
-      :rate="item.price"
-      :available="available"
-      @update="updateAvailable">
-    </my-counter>
+    <my-component :label="labelA" @update="statea= $event"/>
+    <div>X</div>
+    <my-component :label="labelB" @update="stateb= $event"/>
+    <div>=</div>
+    <my-result>{{result}}</my-result>
   </div>
 </template>
 
 <script>
-import MyCounter from './components/MyCounter.vue'
+import MyComponent from "./components/MyComponent.vue";
+import MyResult from "./components/MyResult.vue";
 
 export default {
-  name: 'app',
+  name: "app",
   components: {
-    MyCounter
+    MyComponent,
+    MyResult
   },
-  data () {
+  data() {
     return {
       available: 100,
-      items: []
+      items: [],
+      labelA: "A",
+      labelB: "B",
+      statea: 0,
+      stateb: 0
+    };
+  },
+  computed: {
+    result() {
+      return this.statea * this.stateb;
     }
   },
-  methods: {
-    log (msg) {
-      console.log(msg)
-    },
-    updateAvailable (value) {
-      this.available = this.available - value
-    },
-    fetchItems () {
-      setTimeout(() => {
-        console.log('Items fetched')
-        this.items = [
-          { label: 'Apple', price: 5 },
-          { label: 'Banana', price: 10 },
-          { label: 'Coconut', price: 20 }
-        ]
-      }, 1000)
-    }
-  },
-  created () {
-    this.fetchItems()
+  created() {
+    this.fetchItems();
   }
-}
+};
 </script>
 
 <style lang="scss">
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
   font-size: 36px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  & > div + div {
+    margin-left: 30px;
+  }
 
   .counter {
     margin-bottom: 1em;
